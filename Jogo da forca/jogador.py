7f0ca274e77cef5d73a3b60d55a183710c7d755d
ephiguxta <fabiogustavo@tuta.io>
2025-06-22 12:39:41 -0400

    refatorando a montagem do boneco pra ser mostrado de acordo com o argumento "chances"

--- Jogo da forca/jogador.py
+++ Jogo da forca/jogador.py
@@ -1,21 +1,33 @@
 import socket, time
+from ast import literal_eval
+
+def mostrarMensagem(msg):
+    # como o dado enviado é uma representação de lista em string e tem
+    # alguns dados a mais no começo, devemos removêlos
+    indiceList = msg.index('[')
+    msg = msg[indiceList:]
+
+    msg = literal_eval(msg)
+    for char in msg:
+        print(f"{char}", end="")
+
+# Endereço IP e PORTA do servidor
+# SERVER_IP = '10.25.2.154'
+SERVER_IP = '0.0.0.0'
+SERVER_PORTA = 9998
 
 cliente = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 cliente_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-cliente_tcp.connect(('10.25.2.154',9998))
+cliente_tcp.connect((SERVER_IP, SERVER_PORTA - 1))
 print(cliente_tcp.recv(1024).decode())
 tema = input("Digite o número do tema (1-3): ").strip()
 cliente_tcp.send(tema.encode())
-cliente.sendto("Iniciar".encode(), ('10.25.2.154',9999))
+cliente.sendto("Iniciar".encode(), (SERVER_IP, SERVER_PORTA))
 
 while True:
     data, _ = cliente.recvfrom(1024)
     mensagem = data.decode()
-    print(mensagem)
+    mostrarMensagem(mensagem)
     letra = input("Letra: ").strip().lower()
-    cliente.sendto(letra.encode(), ('10.25.2.154',9999))#o servidor não local é ('10.25.2.154', 9999)
+    cliente.sendto(letra.encode(), (SERVER_IP, SERVER_PORTA))#o servidor não local é ('10.25.2.154', 9999)
 
-
-
-#Ip do cliente: 10.25.3.194
-#ip servidor 10.25.2.154
--- Jogo da forca/robo.py
+++ Jogo da forca/robo.py
@@ -1,68 +1,30 @@
 import socket 
 import random
 
+# Endereço IP e PORTA do servidor
+# SERVER_IP = '10.25.2.154'
+SERVER_IP = '0.0.0.0'
+SERVER_PORTA = 9998
+
 # Palavras por categoria
 animal = [['c', 'o', 'a', 'l', 'a'], ['r', 'a', 'p', 'o', 's', 'a'], ['i', 'g', 'u', 'a', 'n', 'a']]
 comida = [['p', 'i', 'p', 'o', 'c', 'a'], ['c', 'a', 'v', 'i', 'a', 'r'], ['m', 'o', 's', 't', 'a', 'r', 'd', 'a']]
 pais = [['c', 'a', 'n', 'a', 'd', 'á'], ['h', 'u', 'n', 'g', 'r', 'i', 'a'], ['b', 'a', 'h', 'a', 'm', 'a', 's']]
 
+# TODO: enviar apenas os dados de montagem do boneco, o cliente é
+#       quem pega a estrutura e desenha localmente.
+# TODO: pensar num jeito de limitar o argumento para 1 - 6
 def desenhaBoneco(chances):
-    bonecos = [
-        """
-        --------
-        |      |
-        |      
-        |      
-        |      
-        |      
-        --------
-        """,
-        """
-        --------
-        |      |
-        |      O
-        |      
-        |      
-        |      
-        --------
-        """,
-        """
-        --------
-        |      |
-        |      O
-        |      |
-        |      
-        |      
-        --------
-        """,
-        """
-        --------
-        |      |
-        |      O
-        |     /|
-        |      
-        |      
-        --------
-        """,
-        """
-        --------
-        |      |
-        |      O
-        |     /|\\
-        |      
-        |      
-        --------
-        """,
-        """
-        --------
-        |      |
-        |      O
-        |     /|\\
-        |     / 
-        |      
-        --------
-        """,
-        """
+    # TODO: talvez sejam números hardcodeds, refatorar essa seção?!
+    membrosIndices = [50, 67, 66, 68, 84, 86]
+    cabeca = 50
+    torax = 67
+    bracoEsq = 66
+    bracoDir = 68
+    pernaEsq = 84
+    pernaDir = 86
+
+    bonecoStr = """
         --------
         |      |
         |      O
@@ -71,8 +33,50 @@ def desenhaBoneco(chances):
         |      
         --------
         """
-    ]
-    return bonecos[6 - chances]
+
+    boneco = list(bonecoStr)
+    bonecoCompleto = boneco
+
+    # sumindo com os membros, a variável 'bonecoStr' serve
+    # apenas como visualização do corpo completo
+    for i in membrosIndices:
+        boneco[i] = " "
+
+    # que falta faz um switch case
+    if chances == 6:
+        return boneco
+
+    elif chances == 5:
+        boneco[cabeca] = "O"
+        return boneco
+
+    elif chances == 4:
+        boneco[cabeca] = "O"
+        boneco[torax] = "|"
+        return boneco
+
+    elif chances == 3:
+       boneco[cabeca] = "O"
+       boneco[torax] = "|"
+       boneco[bracoEsq] = "/"
+       return boneco
+
+    elif chances == 2:
+       boneco[cabeca] = "O"
+       boneco[torax] = "|"
+       boneco[bracoEsq] = "/"
+       boneco[bracoDir] = "\\"
+       return boneco
+
+    elif chances == 1:
+       boneco[cabeca] = "O"
+       boneco[torax] = "|"
+       boneco[bracoEsq] = "/"
+       boneco[bracoDir] = "\\"
+       boneco[pernaEsq] = "/"
+       return boneco
+
+    return bonecoCompleto
 
 def caixinha(texto):
     linhas = texto.strip().split('\n')
@@ -83,9 +87,9 @@ def caixinha(texto):
 
 # Iniciar socket
 servidor = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-servidor.bind(('10.25.2.154', 9999))
+servidor.bind((SERVER_IP, SERVER_PORTA))
 servidor_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-servidor_tcp.bind(('10.25.2.154', 9998))
+servidor_tcp.bind((SERVER_IP, SERVER_PORTA - 1))
 servidor_tcp.listen(1)
 conn, addr = servidor_tcp.accept()
 grupos = " ".join("Escolha um tema: \n[1] Animal \n[2]Comida \n[3]País")
@@ -167,5 +171,4 @@ while True:
         caixinha(mensagem)
         servidor.sendto(mensagem.encode(), address)
         servidor.close()
-        
-        
+
